f346f06918f72f375e69d4f062642720da8389a8
AnjaliSharma2002 <sharma.anjali366953@gmail.com>
2025-07-14 23:42:39 +0530

    This is my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Green_vision",
+    version="0.0.1",
+    author="anjali",
+    author_email="abc@sh.live",
+    packages=find_packages(),
+    intall_requires = []
+)
